975a058235d37f61d00382908ca322a95fb44086
filipton <filipton12@gmail.com>
2024-03-22 09:42:10 +0100

    chore: perf

---
+++ perf.js
@@ -0,0 +1,17 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+
+export const options = {
+    stages: [
+        { duration: "5s", target: 100 },
+        { duration: "30s", target: 100 },
+        { duration: "5s", target: 0 },
+    ],
+};
+
+// Simulated user behavior
+export default function() {
+    let res = http.get("http://dsa2.fkm.filipton.space");
+    // Validate response status
+    check(res, { "status was 200": (r) => r.status == 200 });
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -6,36 +6,44 @@ use tokio::{
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let listener = TcpListener::bind("0.0.0.0:1337").await?;
-    loop {
-        let (mut stream, _) = listener.accept().await?;
-        let mut client = TcpStream::connect("127.0.0.1:1069").await?;
-        let mut in_buffer = [0; 8192];
+    let addr = std::env::args().nth(1).unwrap_or("1337".to_string());
 
-        let n = stream.read(&mut in_buffer).await?;
-        let mut start = 0;
-        let mut stop = 0;
-        for i in 0..n {
-            if in_buffer[i] == b'\n' {
-                if start == 0 {
-                    start = i + 1;
-                    continue;
-                } else if stop == 0 {
-                    stop = i - 1;
-                    break;
-                }
+    let listener = TcpListener::bind(addr).await?;
+    loop {
+        let (stream, _) = listener.accept().await?;
+        _ = tokio::task::spawn(handle_client(stream)).await?;
+    }
+
+    //Ok(())
+}
+
+async fn handle_client(mut stream: TcpStream) -> Result<()> {
+    let mut client = TcpStream::connect("127.0.0.1:1069").await?;
+    let mut in_buffer = [0; 8192];
+
+    let n = stream.read(&mut in_buffer).await?;
+    let mut start = 0;
+    let mut stop = 0;
+    for i in 0..n {
+        if in_buffer[i] == b'\n' {
+            if start == 0 {
+                start = i + 1;
+                continue;
+            } else if stop == 0 {
+                stop = i - 1;
+                break;
             }
         }
-
-        // Skip "Host: " part of host header (to get host only)
-        start += 6;
-
-        let str = String::from_utf8_lossy(&in_buffer[start..stop]);
-        println!("str: |{str}|");
-
-        client.write(&in_buffer).await?;
-        tokio::io::copy_bidirectional(&mut stream, &mut client).await?;
     }
+
+    // Skip "Host: " part of host header (to get host only)
+    start += 6;
+
+    let str = String::from_utf8_lossy(&in_buffer[start..stop]);
+    //println!("str: |{str}|");
+
+    client.write(&in_buffer).await?;
+    tokio::io::copy_bidirectional(&mut stream, &mut client).await?;
 
     Ok(())
 }

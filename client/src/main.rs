9d17b514f2a48a82d9408277c895ecbdad7a8c57
filipton <filipton12@gmail.com>
2024-03-23 21:31:59 +0100

    wip: client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+};
+
+const SERVER_ADDR: &str = "127.0.0.1:6969";
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut connector = TcpStream::connect(SERVER_ADDR).await?;
+    let mut conn_buff = [0u8; 64];
+    conn_buff[0] = 0x00; // tell the server we are the connector
+
+    connector.write_all(&conn_buff).await?;
+    loop {
+        let res = connector.read_u8().await?;
+        if res == 0x40 {
+            tokio::task::spawn(spawn_tunnel());
+        }
+    }
+
+    //Ok(())
+}
+
+async fn spawn_tunnel() -> Result<()> {
+    let mut local_stream = TcpStream::connect("127.0.0.1:5000").await?;
+    let mut tunnel_stream = TcpStream::connect(SERVER_ADDR).await?;
+    local_stream.set_nodelay(true)?;
+
+    let mut conn_buff = [0u8; 64];
+    conn_buff[0] = 0x01; // tell the server we are the tunnel
+    tunnel_stream.write_all(&conn_buff).await?;
+    tunnel_stream.set_nodelay(true)?;
+
+    println!("Tunnel created");
+    tokio::io::copy_bidirectional(&mut local_stream, &mut tunnel_stream).await?;
+    Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,24 +1,103 @@
 use anyhow::Result;
+use kanal::{AsyncReceiver, AsyncSender};
+use std::{collections::HashMap, sync::Arc};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
+    sync::RwLock,
 };
+
+pub type OpenTunnChan = Arc<
+    RwLock<
+        HashMap<
+            String,
+            (
+                AsyncSender<()>,
+                AsyncSender<TcpStream>,
+                AsyncReceiver<TcpStream>,
+            ),
+        >,
+    >,
+>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let addr = std::env::args().nth(1).unwrap_or("1337".to_string());
+    let connector_addr = "0.0.0.0:6969".to_string();
 
+    let open_tunnel_chan: OpenTunnChan = Arc::new(RwLock::new(HashMap::new()));
+
+    tokio::task::spawn(remote_listener(addr, open_tunnel_chan.clone()));
+    tokio::task::spawn(connector_listener(connector_addr, open_tunnel_chan.clone()));
+
+    let mut sigterm = tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())?;
+    tokio::select! {
+        _ = sigterm.recv() => {
+            println!("Received SIGTERM, stopping server!");
+        }
+        _ = tokio::signal::ctrl_c() => {
+            println!("Received SIGINT, stopping server!");
+        }
+    }
+    Ok(())
+}
+
+async fn connector_listener(addr: String, open_tunnel_chanel: OpenTunnChan) -> Result<()> {
+    println!("Connector listening on: {addr}");
+    let listener = TcpListener::bind(addr).await?;
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+        tokio::task::spawn(connector_handler(stream, open_tunnel_chanel.clone()));
+    }
+}
+
+async fn connector_handler(mut stream: TcpStream, open_tunnel_channel: OpenTunnChan) -> Result<()> {
+    let mut connection_buff = [0u8; 64];
+    stream.read_exact(&mut connection_buff).await?;
+
+    // im the connector!
+    if connection_buff[0] == 0 {
+        println!("New connector");
+        let (tx, rx) = kanal::unbounded_async::<()>();
+        let (stream_tx, stream_rx) = kanal::unbounded_async();
+
+        {
+            open_tunnel_channel
+                .write()
+                .await
+                .insert("dsa".to_string(), (tx, stream_tx, stream_rx));
+        }
+
+        loop {
+            let _ = rx.recv().await?;
+            println!("Send new tunnel req");
+            stream.write_u8(0x40).await?; // 0x40 - open tunnel
+        }
+    } else if connection_buff[0] == 1 {
+        // im the tunnel!
+        let tun_dest = "dsa"; // read from connection_buff
+        let open_tunnel_channel = open_tunnel_channel.read().await;
+        let tx = &open_tunnel_channel.get(tun_dest).unwrap().1;
+
+        stream.set_nodelay(true)?;
+        tx.send(stream).await?;
+        println!("new tunnel conn");
+    }
+
+    Ok(())
+}
+
+async fn remote_listener(addr: String, open_tunnel_channel: OpenTunnChan) -> Result<()> {
+    println!("Remote listening on: {addr}");
     let listener = TcpListener::bind(addr).await?;
     loop {
         let (stream, _) = listener.accept().await?;
-        _ = tokio::task::spawn(handle_client(stream)).await?;
+        tokio::task::spawn(handle_client(stream, open_tunnel_channel.clone()));
     }
-
-    //Ok(())
 }
 
-async fn handle_client(mut stream: TcpStream) -> Result<()> {
-    let mut client = TcpStream::connect("127.0.0.1:1069").await?;
+async fn handle_client(mut stream: TcpStream, open_tunnel_channel: OpenTunnChan) -> Result<()> {
     let mut in_buffer = [0; 8192];
 
     let n = stream.read(&mut in_buffer).await?;
@@ -39,11 +118,19 @@ async fn handle_client(mut stream: TcpStream) -> Result<()> {
     // Skip "Host: " part of host header (to get host only)
     start += 6;
 
-    let str = String::from_utf8_lossy(&in_buffer[start..stop]);
-    //println!("str: |{str}|");
+    let host = String::from_utf8_lossy(&in_buffer[start..stop]);
+    println!("str: |{host}|");
+    let host = "dsa"; // for test
 
-    client.write(&in_buffer).await?;
-    tokio::io::copy_bidirectional(&mut stream, &mut client).await?;
+    let chan = open_tunnel_channel.read().await;
+    let (tx, _s_tx, s_rx) = chan.get(host).unwrap();
+    tx.send(()).await?;
+
+    let mut tunnel = s_rx.recv().await?;
+    tunnel.write(&in_buffer).await?;
+    stream.set_nodelay(true)?;
+    println!("Got new tunnel");
+    tokio::io::copy_bidirectional(&mut stream, &mut tunnel).await?;
 
     Ok(())
 }

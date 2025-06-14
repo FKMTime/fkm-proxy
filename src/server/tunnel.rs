0290e578e24fca7fd2a5921e122f796bf0d1fd5b
filipton <filipton12@gmail.com>
2025-06-14 16:36:03 +0200

    feat: server print close tunnel reason

--- src/server/tunnel.rs
+++ src/server/tunnel.rs
@@ -134,6 +134,7 @@ async fn connector_loop(
                         _ = send_string_to_stream(stream, &reason).await;
                         _ = stream.flush().await;
 
+                        tracing::warn!("Closing tunnel with reason: {reason}");
                         return Ok(false)
                     },
                     TunnelRequest::Request { ssl, tunnel_id } => {
